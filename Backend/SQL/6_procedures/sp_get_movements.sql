658ab76f5b84ea76f537d26bc74c445fde4d9b90
GilbertAb <gilbertma2000@gmail.com>
2025-08-16 19:13:49 -0600

    feat(sp): return error's resultset in usp_GetMovements

--- Backend/SQL/6_procedures/sp_get_movements.sql
+++ Backend/SQL/6_procedures/sp_get_movements.sql
@@ -15,14 +15,30 @@ GO
 CREATE PROCEDURE MyInventory.usp_GetMovements
 AS
 BEGIN
-	SELECT
-		Id,
-		ProductId,
-		MovementTypeId,
-		MovementDate,
-		Quantity,
-		MovementDescription,
-		CreatedAt
-	FROM MyInventory.Movement;
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+		-- Get Movements data
+		SELECT
+			Id,
+			ProductId,
+			MovementTypeId,
+			MovementDate,
+			Quantity,
+			MovementDescription,
+			CreatedAt
+		FROM MyInventory.Movement;
+
+		-- Succeed data
+		SELECT 
+			0 AS ErrorCode,
+			'OK' AS ErrorMessage;
+	END TRY
+	BEGIN CATCH
+		SELECT
+			ERROR_NUMBER() AS ErrorCode,
+			ERROR_MESSAGE() AS ErrorMessage;
+	END CATCH
+	
 END;
 GO
