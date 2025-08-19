9b12f593104e8f2e94d1aea9f992871200d7f399
GilbertAb <gilbertma2000@gmail.com>
2025-08-19 00:25:53 -0600

    feat(sp): add movement validations

--- Backend/SQL/6_procedures/sp_get_product_stock_history.sql
+++ Backend/SQL/6_procedures/sp_get_product_stock_history.sql
@@ -19,33 +19,68 @@ BEGIN
 	SET NOCOUNT ON;
 	
 	-- Error data
-    DECLARE 
+    DECLARE
         @ErrorCode INT = 0,
         @ErrorMessage NVARCHAR(200) = 'OK';
 
-	BEGIN TRY
-		-- Verify if the product exists
-		IF NOT EXISTS (SELECT 1 FROM MyInventory.Product WHERE Id = @ProductId)
-		BEGIN
-			SET @ErrorCode = 50001;
-			SET @ErrorMessage = 'Product not found';
-		END
-		
-		-- Get movement (most recent to oldest)
-		IF @ErrorCode = 0
+	-- Verify if the product exists
+	IF NOT EXISTS (SELECT 1 FROM MyInventory.Product WHERE Id = @ProductId)
+	BEGIN
+		-- Empty data
 		SELECT 
-			Id AS MovementId,
-			ProductId,
-			MovementTypeId,
-			MovementDate,
-			Quantity,
-			MovementDescription,
-			CreatedAt
-		FROM MyInventory.Movement
-		WHERE ProductId = @ProductId
-		ORDER BY MovementDate DESC;
+			NULL AS MovementId,
+			NULL AS ProductId,
+			NULL AS MovementTypeId,
+			NULL AS MovementDate,
+			NULL AS Quantity,
+			NULL AS MovementDescription,
+			NULL AS CreatedAt;
+		-- Error data
+		SET @ErrorCode = 50001;
+		SET @ErrorMessage = 'Product not found';
+	END
+	-- Verify if a movement exists
+	ELSE IF NOT EXISTS (SELECT 1 FROM MyInventory.Movement WHERE ProductId = @ProductId)
+	BEGIN
+		-- Empty data
+		SELECT 
+			NULL AS MovementId,
+			NULL AS ProductId,
+			NULL AS MovementTypeId,
+			NULL AS MovementDate,
+			NULL AS Quantity,
+			NULL AS MovementDescription,
+			NULL AS CreatedAt;
+		-- Error data
+		SET @ErrorCode = 50101;
+		SET @ErrorMessage = 'No movements found';
+	END
+	ELSE
+	BEGIN TRY
+		-- Get movement (most recent to oldest)
+		SELECT 
+            Id AS MovementId,
+            ProductId,
+            MovementTypeId,
+            MovementDate,
+            Quantity,
+            MovementDescription,
+            CreatedAt
+        FROM MyInventory.Movement
+        WHERE ProductId = @ProductId
+        ORDER BY MovementDate DESC;
 	END TRY
 	BEGIN CATCH
+		-- Empty data
+		SELECT 
+			NULL AS MovementId,
+			NULL AS ProductId,
+			NULL AS MovementTypeId,
+			NULL AS MovementDate,
+			NULL AS Quantity,
+			NULL AS MovementDescription,
+			NULL AS CreatedAt;
+		-- Error data
 		SET @ErrorCode = ERROR_NUMBER();
         SET @ErrorMessage = ERROR_MESSAGE();
 	END CATCH;
