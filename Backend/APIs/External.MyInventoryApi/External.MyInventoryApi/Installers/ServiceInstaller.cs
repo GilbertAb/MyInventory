54ea4fe1f7c46f08db55b47baf63bea4bcf9ad4a
GilbertAb <gilbertma2000@gmail.com>
2025-08-23 03:21:07 -0600

    chore(api): add ServiceInstaller to register dependencies

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi/Installers/ServiceInstaller.cs
@@ -0,0 +1,22 @@
+﻿using External.MyInventoryApi.Application.Contracts.Services;
+using External.MyInventoryApi.Application.Services;
+using External.MyInventoryApi.CrossCutting.Contracts;
+using External.MyInventoryApi.CrossCutting.Crypto;
+using External.MyInventoryApi.DataAccess.Contracts.Repositories;
+using External.MyInventoryApi.DataAccess.Contracts.SqlServer;
+using External.MyInventoryApi.DataAccess.Repositories;
+using External.MyInventoryApi.DataAccess.SqlServer;
+
+namespace External.MyInventoryApi.Installers
+{
+    public static class ServiceInstaller
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ISqlServerDatabase, SqlServerDatabase>();
+            services.AddSingleton<ICrypto, Crypto>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
