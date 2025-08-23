4baa6a773dc10c006d04b21bc7b7b6db24ef3964
GilbertAb <gilbertma2000@gmail.com>
2025-08-23 02:50:00 -0600

    feat(Application): add ProductDto. Add ServiceResult model. Add Interface of Product Service

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Application.Contracts/Results/ServiceResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace External.MyInventoryApi.Application.Contracts.Results
+{
+    public class ServiceResult<T>
+    {
+        public T? Data { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+
+        public static ServiceResult<T> Success(T data) =>
+            new ServiceResult<T> { Data = data, ErrorCode = 0, ErrorMessage = "OK" };
+
+        public static ServiceResult<T> Fail(int errorCode, string errorMessage) =>
+            new ServiceResult<T> { Data = default, ErrorCode = errorCode, ErrorMessage = errorMessage };
+    }
+}
---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Application.Contracts/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using External.MyInventoryApi.Application.Contracts.DTOs;
+using External.MyInventoryApi.Application.Contracts.Results;
+
+namespace External.MyInventoryApi.Application.Contracts.Services
+{
+    public interface IProductService
+    {
+        public Task<ServiceResult<int?>> AddProduct(ProductDto product);
+        public Task<ServiceResult<IEnumerable<ProductDto>?>> GetAllProducts();
+    }
+}
