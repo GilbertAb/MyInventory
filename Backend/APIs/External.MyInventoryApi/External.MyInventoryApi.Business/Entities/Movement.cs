d8fd2ff1f155825d7ee0749c3a23f8518693d5b0
GilbertAb <gilbertma2000@gmail.com>
2025-08-23 02:06:25 -0600

    feat(business): add Product and Movement entities to domain

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Business/Entities/Movement.cs
@@ -0,0 +1,13 @@
+﻿namespace External.MyInventoryApi.Business.Entities
+{
+    public class Movement
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int MovementTypeId { get; set; }
+        public int MovementDate { get; set; }
+        public int Quantity { get; set; }
+        public string? MovementDescription { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
