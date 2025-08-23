1313aad61b885353fb766e4b3be1af7e3691f98e
GilbertAb <gilbertma2000@gmail.com>
2025-08-23 02:12:33 -0600

    feat(DataAccess): Interface for SQL Server database and models for results

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.DataAccess.Contracts/SqlServer/ISqlServerDatabase.cs
@@ -0,0 +1,14 @@
+﻿using External.MyInventoryApi.DataAccess.Contracts.SqlServer.Results;
+using System.Data;
+
+namespace External.MyInventoryApi.DataAccess.Contracts.SqlServer
+{
+    public interface ISqlServerDatabase : IDisposable
+    {
+        // Execute
+        Task<StoredProcedureResult<DataSet>> ExecuteAsync(string storedProcedureName, Dictionary<string, object> parameters);
+
+        // Health
+        Task<bool> IsHealthyAsync();
+    }
+}
