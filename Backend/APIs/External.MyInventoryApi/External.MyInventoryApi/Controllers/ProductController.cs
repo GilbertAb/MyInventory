511410880949f77d30e6a79bfb971fe17c1bef15
GilbertAb <gilbertma2000@gmail.com>
2025-08-23 03:11:31 -0600

    feat(Application): add Product Controller. Add http get getProducts operation

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using External.MyInventoryApi.Application.Contracts.DTOs;
+using External.MyInventoryApi.Application.Contracts.Results;
+using External.MyInventoryApi.Application.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace External.MyInventoryApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        [HttpGet("getProducts")]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            // Call get all products service
+            ServiceResult<IEnumerable<ProductDto>?> result = await _productService.GetAllProducts();
+
+            if (result.ErrorCode != 0)
+            {
+                return BadRequest(new { result.ErrorCode, result.ErrorMessage });
+            }
+
+            return Ok(result);
+        }
+    }
+}
