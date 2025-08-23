e89cec46249e40227093670df159d6edfe581fac
GilbertAb <gilbertma2000@gmail.com>
2025-08-23 03:46:41 -0600

    chore(api): add api configuration

--- Backend/APIs/External.MyInventoryApi/External.MyInventoryApi/Program.cs
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi/Program.cs
@@ -1,17 +1,26 @@
+
+using External.MyInventoryApi.Installers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuración
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                     .AddEnvironmentVariables();
 
+// Servicios
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.RegisterServices();
 
+// Build
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// TODO: Middleware
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+// Endpoints
 app.MapControllers();
 
 app.Run();
