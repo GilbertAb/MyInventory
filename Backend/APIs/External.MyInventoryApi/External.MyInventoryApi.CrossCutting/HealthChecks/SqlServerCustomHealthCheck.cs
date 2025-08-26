9d11fb0f151be08976b998e2bfe12803fd12a4b1
GilbertAb <gilbertma2000@gmail.com>
2025-08-25 20:33:52 -0600

    feature(Api): implemented HealthCheck for Sql Server database. Create HealthCheck installer

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.CrossCutting/HealthChecks/SqlServerCustomHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using External.MyInventoryApi.DataAccess.Contracts.SqlServer;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace External.MyInventoryApi.CrossCutting.HealthChecks
+{
+    public class SqlServerCustomHealthCheck : IHealthCheck
+    {
+        private readonly ISqlServerDatabase _database;
+
+        public SqlServerCustomHealthCheck(ISqlServerDatabase database)
+        {
+            _database = database ?? throw new ArgumentNullException(nameof(database));
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                bool isHealthy = await _database.IsHealthyAsync();
+
+                if (!isHealthy)
+                {
+                    return HealthCheckResult.Unhealthy("SQL Server connection is not healthy");
+                }
+
+                return HealthCheckResult.Healthy("SQL Server is reachable");
+            } catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("SQL Server check failed", ex);
+            }
+        }
+    }
+}
--- Backend/APIs/External.MyInventoryApi/External.MyInventoryApi/Program.cs
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi/Program.cs
@@ -1,6 +1,8 @@
 
 using External.MyInventoryApi.CrossCutting.Middleware;
 using External.MyInventoryApi.Installers;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnCh
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.RegisterServices();
+builder.Services.RegisterHealthChecks();
 
 // Build
 var app = builder.Build();
@@ -25,5 +28,24 @@ app.UseAuthorization();
 
 // Endpoints
 app.MapControllers();
+
+app.MapHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var response = new
+        {
+            Status = report.Status.ToString(),
+            Checks = report.Entries.Select(x => new {
+                Component = x.Key,
+                Status = x.Value.Status.ToString(),
+                Description = x.Value.Description
+            }),
+            Duration = report.TotalDuration.ToString()
+        };
+        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+});
 
 app.Run();
