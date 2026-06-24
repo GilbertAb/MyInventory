9c55356832895d1432e804c8f2d7760c8e254b76
GilbertAb <gilbertma2000@gmail.com>
2026-06-23 18:48:23 -0600

    feat(tests): add test for GetAllProducts use case when repository returns data

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Tests/Application/ProductServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using External.MyInventoryApi.Application.Contracts.DTOs;
+using External.MyInventoryApi.Application.Contracts.Services;
+using External.MyInventoryApi.Application.Services;
+using External.MyInventoryApi.Business.Entities;
+using External.MyInventoryApi.DataAccess.Contracts.Repositories;
+using External.MyInventoryApi.DataAccess.Contracts.Results;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace External.MyInventoryApi.Tests.Application
+{
+    public class ProductServiceTests
+    {
+        //
+        private readonly Mock<IProductRepository> _repositoryMock;
+        private readonly ProductService _service;
+
+        public ProductServiceTests()
+        {
+            _repositoryMock = new Mock<IProductRepository>();
+            _service = new ProductService(_repositoryMock.Object);
+        }
+
+        // GetAllProducts use case
+        [Fact]
+        public async Task GetAllProducts_ShouldReturnMappedResult_WhenRepositoryReturnsData()
+        {
+            // Arrange
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    ProductName = "Test",
+                    Stock = 10,
+                    Category = "Test"
+                },
+                new Product
+                {
+                    Id = 1,
+                    ProductName = "Test",
+                    Stock = 10,
+                    Category = "Test"
+                }
+            };
+            //OperationResult<IEnumerable<Product>?>
+            var operationResult = new OperationResult<IEnumerable<Product>?>
+            {
+                Data = products,
+                ErrorCode = 0,
+                ErrorMessage = "Succeed"
+            };
+
+            _repositoryMock
+                .Setup(r => r.GetAllProducts())
+                .ReturnsAsync(operationResult);
+
+
+            // Act
+            var result = await _service.GetAllProducts();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.ErrorCode.Should().Be(0);
+            result.Data.Should().HaveCount(2);
+            result.Data.Should().BeEquivalentTo(products, options =>
+                options
+                    .Excluding(x => x.CreatedAt)
+                    .Excluding(x => x.UpdatedAt)
+            );
+
+            _repositoryMock.Verify(r => r.GetAllProducts(), Times.Once());
+        }
+
+    }
+}
