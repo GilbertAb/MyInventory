66813063e641aaf8cf8dda89168e1104a41f4430
GilbertAb <gilbertma2000@gmail.com>
2026-06-29 08:36:19 -0600

    feat(tests): add tests for GetProductStockHistory  use case

--- Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Tests/Application/MovementServiceTests.cs
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Tests/Application/MovementServiceTests.cs
@@ -215,5 +215,113 @@ namespace External.MyInventoryApi.Tests.Application
             result.ErrorCode.Should().Be(500);
             result.ErrorMessage.Should().Be("Database error");
         }
+
+        /*
+         *---------------------------------------------------------
+         *---------------| GetProductStockHistory use case |-----------------
+         *---------------------------------------------------------
+        */
+        [Fact]
+        public async Task GetProductStockHistory_ShouldReturnMappedResult_WhenRepositoryReturnsData()
+        {
+            // Arrange
+            var movements = new List<Movement>
+            {
+                new Movement
+                {
+                    ProductId = 1,
+                    MovementTypeId = 1,
+                    MovementDate = DateTime.Now,
+                    Quantity = 5,
+                    MovementDescription = "movement"
+                },
+                new Movement
+                {
+                    ProductId = 1,
+                    MovementTypeId = 2,
+                    MovementDate = DateTime.Now,
+                    Quantity = 15,
+                    MovementDescription = "IN"
+                }
+            };
+
+            var operationResult = new OperationResult<IEnumerable<Movement>?>
+            {
+                Data = movements,
+                ErrorCode = 0,
+                ErrorMessage = string.Empty
+            };
+
+
+            _repositoryMock
+                .Setup(r => r.GetProductStockHistory(12))
+                .ReturnsAsync(operationResult);
+
+            // Act
+            var result = await _service.GetProductStockHistory(12);
+
+
+            // Assert
+
+            result.Data.Should().NotBeNull();
+            result.ErrorCode.Should().Be(0);
+
+            result.Data.Should().BeEquivalentTo(movements, options =>
+                options
+                    .Excluding(x => x.CreatedAt)
+            );
+
+            _repositoryMock.Verify(r => r.GetProductStockHistory(12), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetProductStockHistory_ShouldReturnEmptyCollection_WhenNoMovementsExist()
+        {
+            // Arrange
+            var operationResult = new OperationResult<IEnumerable<Movement>?>
+            {
+                Data = [],
+                ErrorCode = 0
+            };
+
+            _repositoryMock
+                .Setup(r => r.GetProductStockHistory(12))
+                .ReturnsAsync(operationResult);
+
+            // Act
+            var result = await _service.GetProductStockHistory(12);
+
+            // Assert
+            result.Data.Should().BeEmpty();
+            result.ErrorCode.Should().Be(0);
+
+            _repositoryMock.Verify(r => r.GetProductStockHistory(12), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetProductStockHistory_ShouldReturnError_WhenRepositoryReturnsError()
+        {
+            // Arrange
+            var operationResult = new OperationResult<IEnumerable<Movement>?>
+            {
+                Data = null,
+                ErrorCode = 500,
+                ErrorMessage = "Database error"
+            };
+
+            _repositoryMock
+                .Setup(r => r.GetMovements())
+                .ReturnsAsync(operationResult);
+
+
+            // Act
+            var result = await _service.GetMovements();
+
+            // Assert
+            result.ErrorCode.Should().Be(500);
+            result.ErrorMessage.Should().Be("Database error");
+
+            _repositoryMock.Verify(r => r.GetMovements(), Times.Once());
+        }
     }
 }
