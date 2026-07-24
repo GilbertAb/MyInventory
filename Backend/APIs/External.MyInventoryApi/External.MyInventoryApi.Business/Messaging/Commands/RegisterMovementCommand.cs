228319bb2930abec8785c0e1dd695f65eb7e6f53
GilbertAb <gilbertma2000@gmail.com>
2026-07-23 20:39:06 -0600

    feat(messaging): add RegisterMovementCommand

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Business/Messaging/Commands/RegisterMovementCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace External.MyInventoryApi.Business.Messaging.Commands
+{
+    public sealed record RegisterMovementCommand
+    {
+        public int ProductId { get; set; }
+        public byte MovementTypeId { get; set; }
+        public int Quantity { get; set; }
+        public string MovementDescription { get; set; } = string.Empty;
+    }
+}
