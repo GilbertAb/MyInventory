3414e8e3ba9222acc71476d3861e9ad767264570
GilbertAb <gilbertma2000@gmail.com>
2026-07-30 01:37:04 -0600

    feat(tests): add tests for Application layer models

---
+++ Backend/APIs/External.MyInventoryApi/External.MyInventoryApi.Tests/Application/ModelsTests.cs
@@ -0,0 +1,37 @@
+﻿using External.MyInventoryApi.Application.Contracts.Results;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace External.MyInventoryApi.Tests.Application
+{
+    public class ModelsTests
+    {
+        [Fact]
+        public void Success_ShouldReturnServiceResultWithSuccessValues()
+        {
+            // Act
+            var result = ServiceResult<int>.Success(123);
+
+            // Assert
+            result.Data.Should().Be(123);
+            result.ErrorCode.Should().Be(0);
+            result.ErrorMessage.Should().Be("OK");
+        }
+
+        [Fact]
+        public void Fail_ShouldReturnServiceResultWithFailValues()
+        {
+            // Act
+            var result = ServiceResult<int>.Fail(321,"Wrong value");
+
+            // Assert
+            result.Data.Should().Be(default);
+            result.ErrorCode.Should().Be(321);
+            result.ErrorMessage.Should().Be("Wrong value");
+        }
+    }
+}
